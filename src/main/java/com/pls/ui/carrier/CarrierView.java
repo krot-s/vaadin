5b634eaf46f07b12ab5cc15d0fa4bb45b37d7f67
Alexander Nalapko <analapko@plslogistics.com>
2012-08-06 13:17:05 +0300

    Added editable table

--- src/main/java/com/pls/ui/carrier/CarrierView.java
+++ src/main/java/com/pls/ui/carrier/CarrierView.java
@@ -1,10 +1,16 @@
 package com.pls.ui.carrier;
 
+import java.util.Date;
+
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
+import com.pls.domain.Carrier;
+import com.pls.domain.CarrierStatus;
+import com.pls.ui.components.CustomTable;
 import com.pls.ui.menu.MainMenu;
 import com.vaadin.Application;
+import com.vaadin.data.util.BeanContainer;
 import com.vaadin.ui.VerticalLayout;
 
 public class CarrierView {
@@ -27,6 +33,26 @@ public class CarrierView {
 	private void initLayout(){
 		VerticalLayout layout = new VerticalLayout();
 		layout.addComponent(mainMenu);
+
+		BeanContainer<Long, Carrier> bean = new BeanContainer<Long, Carrier>(
+				Carrier.class);
+		bean.setBeanIdProperty("id");
+
+		for (int i = 0; i < 10; i++) {
+			Carrier carrier = new Carrier();
+			carrier.setId((long) i);
+			carrier.setContactName(String.valueOf(i));
+			carrier.setMcNumber((long) i);
+			carrier.setName(String.valueOf(i));
+			carrier.setScac(String.valueOf(i));
+			carrier.setStatus(CarrierStatus.ACTIVE);
+			carrier.setTaxId(String.valueOf(i));
+			carrier.setValidUntil(new Date());
+			bean.addBean(carrier);
+		}
+		
+		CustomTable table = new CustomTable("Carrier",bean);
+		layout.addComponent(table);
 		
 		application.getMainWindow().setContent(layout);
 	}
---
+++ src/main/java/com/pls/ui/components/CustomTable.java
@@ -0,0 +1,95 @@
+package com.pls.ui.components;
+
+import java.util.HashMap;
+import com.vaadin.data.Container;
+import com.vaadin.data.util.BeanContainer;
+import com.vaadin.event.FieldEvents.BlurEvent;
+import com.vaadin.event.FieldEvents.BlurListener;
+import com.vaadin.event.FieldEvents.FocusEvent;
+import com.vaadin.event.FieldEvents.FocusListener;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Field;
+import com.vaadin.ui.DefaultFieldFactory;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+public class CustomTable extends CustomComponent {
+
+	private static final long serialVersionUID = 1L;
+	
+	public CustomTable(String tableName , BeanContainer<?, ?> beans) {
+
+		VerticalLayout layout = new VerticalLayout();
+		final Table table = new Table(tableName, beans);
+
+		table.setPageLength(beans.size());
+		table.setColumnReorderingAllowed(true);
+		table.setEditable(true);
+		table.setSelectable(true);
+		table.setContainerDataSource(beans);
+		layout.addComponent(table);
+		setCompositionRoot(layout);
+	
+
+		// Map to find a field component by its item ID and property ID
+		final HashMap<Object, HashMap<Object, Field>> fields = new HashMap<Object, HashMap<Object, Field>>();
+
+		// table.setTableFieldFactory(new TableFieldFactory() {
+		table.setTableFieldFactory(new DefaultFieldFactory() {
+			private static final long serialVersionUID = -5741977060384915110L;
+
+			public Field createField(Container container, final Object itemId,
+					final Object propertyId, Component uiContext) {
+				
+				String typeId = container.getType(propertyId).toString();
+
+				if (typeId.equals(String.class.toString())
+						| typeId.equals(Long.class.toString())) {
+					final TextField tf = new TextField();
+					// Needed for the generated column
+					tf.setImmediate(true);
+					// Manage the field in the field storage
+					HashMap<Object, Field> itemMap = fields.get(itemId);
+
+					if (itemMap == null) {
+						itemMap = new HashMap<Object, Field>();
+						fields.put(itemId, itemMap);
+					}
+					itemMap.put(propertyId, tf);
+
+					tf.setReadOnly(true);
+
+					tf.addListener(new FocusListener() {
+						private static final long serialVersionUID = 1006388127259206641L;
+
+						public void focus(FocusEvent event) {
+							// Make the entire item editable
+							HashMap<Object, Field> itemMap = fields.get(itemId);
+							for (Field f : itemMap.values())
+								f.setReadOnly(false);
+
+							table.select(itemId);
+						}
+					});
+					tf.addListener(new BlurListener() {
+						private static final long serialVersionUID = -4497552765206819985L;
+
+						public void blur(BlurEvent event) {
+							// Make the entire item read-only
+							HashMap<Object, Field> itemMap = fields.get(itemId);
+							for (Field f : itemMap.values())
+								f.setReadOnly(true);
+						}
+					});
+
+					return tf;
+				}
+
+				return super.createField(container, itemId, propertyId,
+						uiContext);
+			}
+		});
+	}
+}
